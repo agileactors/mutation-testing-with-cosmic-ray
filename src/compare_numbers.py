9cb860faed68741530c7247423bfcfeb91781255
george.papadakis <george.papadakis@newcrosshealthcare.com>
2023-07-20 09:35:52 +0300

    Initial implementation

--- src/compare_numbers.py
+++ src/compare_numbers.py
@@ -1,7 +1,7 @@
 def compare_numbers(a, b):
-    if a > b:
+    if a == b:
         return f"{a} is greater than {b}"
-    elif a < b:
+    elif a is not b:
         return f"{a} is less than {b}"
     else:
         return f"{a} is equal to {b}"
