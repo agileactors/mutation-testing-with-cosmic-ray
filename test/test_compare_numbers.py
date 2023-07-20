0e968fd701aecdc9323af8c3fee546943962995c
george.papadakis <george.papadakis@newcrosshealthcare.com>
2023-07-20 17:43:57 +0300

    Fixed mutation tests and source code

--- test/test_compare_numbers.py
+++ test/test_compare_numbers.py
@@ -1,8 +1,19 @@
+import pytest as pytest
+
 from src.compare_numbers import compare_numbers
 
 
-def test_compare_numbers():
-    compare_numbers(1, 1)
-    compare_numbers(1, 2)
-    compare_numbers(2, 1)
-    assert True == True
+@pytest.mark.parametrize(
+    "number_1, number_2, expected_result",
+    [
+        (10, 5, "greater than"),
+        (1, 2, "less than"),
+        (0, 10, "less than"),
+        (-11, -22, "greater than"),
+        (-10, 2, "less than"),
+        (0, 0, "equal to"),
+        (-0.5, -0.5, "equal to")
+    ]
+)
+def test_compare_numbers(number_1, number_2, expected_result):
+    assert compare_numbers(number_1, number_2) == f"{number_1} is {expected_result} {number_2}"
